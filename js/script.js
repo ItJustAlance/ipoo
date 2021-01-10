d4fafce6bfe8359b37f5254beb555a5be5fe886c
ratamanov <r399055750@gmail.com>
2021-01-11 00:37:12 +0500

    новые страницы прошедшее ipo прошедшее ipo таблица карточка компании

--- js/script.js
+++ js/script.js
@@ -35,7 +35,6 @@ $(function() {
 	$(".js-pop-video").on("click", function() {
 		$("#pop-video").fadeIn();
 		$("body").addClass("pop-show");
-
 		return false;
 	});
   $(document).on('click', function(e) {
@@ -45,14 +44,27 @@ $(function() {
   }
     e.stopPropagation();
   });
-  $(document).on('click', function(e) {
-  if (!$(e.target).closest(".b-lang").length) {
-    $(".b-lang .list-lang").slideUp();
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest(".b-lang").length) {
+            $(".b-lang .list-lang").slideUp();
+        }
+        e.stopPropagation();
+    });
 
-  }
-    e.stopPropagation();
-  });
-
+    $(".drop-select__value").on("click", function() {
+        $(this).parent().find('.drop-select__list').slideToggle();
+        $(this).parent().toggleClass("active");
+        return false;
+    });
+/*
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest(".drop-select").length) {
+            $('.drop-select__list').slideUp();
+            $('.drop-select').removeClass("active")
+        }
+        e.stopPropagation();
+    });
+*/
 
 
 // слайдер на главной
@@ -214,53 +226,53 @@ $(window).scroll(function(){
 	}, 200);
 
 	});
+    if($(".countdown").length>0) {
 
+        function getTimeRemaining(endtime) {
+            var t = Date.parse(endtime) - Date.parse(new Date());
+            var seconds = Math.floor((t / 1000) % 60);
+            var minutes = Math.floor((t / 1000 / 60) % 60);
+            var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+            var days = Math.floor(t / (1000 * 60 * 60 * 24));
+            return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+        }
 
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-}
+        function initializeClock(id, endtime) {
+            var clock = document.getElementById(id);
+            var daysSpan = clock.querySelector('.days');
+            var hoursSpan = clock.querySelector('.hours');
+            var minutesSpan = clock.querySelector('.minutes');
+            var secondsSpan = clock.querySelector('.seconds');
 
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+            function updateClock() {
+                var t = getTimeRemaining(endtime);
 
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
+                daysSpan.innerHTML = t.days;
+                hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+                minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+                secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+                if (t.total <= 0) {
+                    clearInterval(timeinterval);
+                }
+            }
 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
+            updateClock();
+            var timeinterval = setInterval(updateClock, 1000);
+        }
+
+        var deadline = "December 30 2020 00:00:00 GMT+0300";
+        // var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+        initializeClock('countdown1', deadline);
+        initializeClock('countdown2', deadline);
+        initializeClock('countdown3', deadline);
     }
-  }
-
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-}
-
-var deadline="December 30 2020 00:00:00 GMT+0300";
-// var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
-initializeClock('countdown1', deadline);
-initializeClock('countdown2', deadline);
-initializeClock('countdown3', deadline);
-
 });
 
 
